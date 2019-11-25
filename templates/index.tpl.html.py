39e4b574a9440181c3e62850e68af38de4c771c9
Chunk3r <p.pirig@freenet.de>
2019-11-25 17:03:43 +0100

    templates hinzugefügt

---
+++ templates/index.tpl.html.py
@@ -0,0 +1,30 @@
+# -*- coding:ascii -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1574697506.921789
+_enable_loop = True
+_template_filename = '/home/pinkman/web/p2/uv/templates/index.tpl.html'
+_template_uri = 'index.tpl.html'
+_source_encoding = 'ascii'
+_exports = []
+
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        __M_writer = context.writer()
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "/home/pinkman/web/p2/uv/templates/index.tpl.html", "uri": "index.tpl.html", "source_encoding": "ascii", "line_map": {"16": 0, "26": 16}}
+__M_END_METADATA
+"""
